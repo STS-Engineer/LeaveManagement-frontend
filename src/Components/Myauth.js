b9b4813c384590291cfc3c7771018d28e6b55c0e
STS-Engineer <administration.sts@avocarbon.com>
2024-08-12 10:00:13 +0100

    Update Myauth.js

--- src/Components/Myauth.js
+++ src/Components/Myauth.js
@@ -14,7 +14,7 @@ const MyAuth = ({ user }) => {
         const token = localStorage.getItem("token");
         const user = JSON.parse(localStorage.getItem("user"));
         const response = await fetch(
-          `http://hr-back-end.azurewebsites.net/authorization-requests/employee/${user.id}`,
+          `https://hr-back-end.azurewebsites.net/authorization-requests/employee/${user.id}`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
